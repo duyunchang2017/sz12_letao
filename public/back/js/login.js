ac9f05d7e15b7c807b2b7dfa1498922a405cb9f0
duyunchang <dyc123@163.com$ git config --global user.email dyc123@163.com>
2017-11-09 20:06:34 +0800

    登录

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -8,6 +8,8 @@ $(function(){
     //密码的长度是6-12位
     //获取到表单
     var $form = $('form');
+    //校验表单
+
     $form.bootstrapValidator({
         //1. 指定不校验的类型，默认为[':disabled', ':hidden', ':not(:visible)'],可以不设置
         // excluded: [':disabled', ':hidden', ':not(:visible)'],
@@ -42,23 +44,55 @@ $(function(){
                     //}
                 }
             },
-           password:{
-               validators: {
-                   //不能为空
-                   notEmpty: {
-                       message: '密码不能为空'
-                   },
-               ///长度校验
-               stringLength: {
-                   min: 6,
-                   max: 12,
-                   message: '密码长度必须在6到12之间'
-               },
-               }
+            password:{
+                validators: {
+                    //不能为空
+                    notEmpty: {
+                        message: '密码不能为空'
+                    },
+                    ///长度校验
+                    stringLength: {
+                        min: 6,
+                        max: 12,
+                        message: '密码长度必须在6到12之间'
+                    },
+                }
 
-           }
+            }
 
         }
+
+    });
+    //给表单注册一个校验成功事件 success.form.bv
+    $form.on('success.form.bv', function (e) {
+        //阻止默认行为
+        e.preventDefault();
+        console.log($form.serialize());
+        //使用ajax发送登录请求
+        $.ajax({
+            url:'/employee/employeeLogin',
+            type:"post",
+            data:$form.serialize(),
+            dataType:'json',//dataType默认是json格式的数据类型，如果
+            //dataType是json格式的数据类型，dataType就可以不写，如果
+            //是text/html,即字符串就需要写
+            success:function(data){
+                //做判断data是一个对象
+                if(data.success){
+                    //跳转到首页
+                    location.href = "index.html";
+                }
+                if(data.error === 1000){
+                   alert('用户名不存在');
+                }
+                if(data.error === 1001 ){
+                    alert('密码错误');
+                }
+
+            }
+
+
+        });
 
     });
 });
